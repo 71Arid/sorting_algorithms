fdd7dcde6a128398bd54b680ed3dbb292d8479b9
71Arid <brian87noel@gmail.com>
2023-08-31 19:26:04 +0300

    {revert}

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,27 +8,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp, k;
-	k = 0;
+	size_t i, j, temp;
 
 	if (!array || size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		if (k == 0)
+		for (j = 0; j < size - 1; j++)
 		{
-			k = 1;
-			for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j+1])
 			{
-				if (array[j] > array[j+1])
-				{
-					temp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = temp;
-					print_array(array, size);
-					k = 0;
-				}
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+				print_array(array, size);
+				k = 0;
 			}
 		}
 	}
